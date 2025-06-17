497b2e41d9072c22f86a5bfc76ffc3da90d87b18
Mona1307 <monicaadhana05@gmail.com>
2025-06-17 18:05:48 +0530

    student sample 1

---
+++ src/Lab_01_multiplication.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.util.Scanner;
+public class Lab_01_multiplication {
+    public static void main(String[] args) {
+        int a, b, c, d, e;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
+        a = sc.nextInt();
+        /*System.out.println("Enter second number");
+        b= sc.nextInt();
+        System.out.println("Enter third number");
+        c= sc.nextInt();
+        System.out.println("Enter fourth number");
+        d= sc.nextInt();*/
+        if(a%2==0)
+        System.out.println("even");
+        else
+        System.out.println("Odd");
+    }
+
+    }
+
--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,10 @@
 public class hello {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("hello team");
-    }git init
+
+        int a=10, b=5,c=0;
+        c=a+b;
+        System.out.println("c="+c);
+    }
 }
