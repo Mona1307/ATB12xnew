9037591e0d116b2c9589e4e0d0f5041eed4d8b81
Mona1307 <monicaadhana05@gmail.com>
2025-06-21 12:09:18 +0530

    student sample 1

---
+++ src/Operator.java
@@ -0,0 +1,22 @@
+public class Operator {
+        public static void main(String[] args) {
+            int a=8,b= 4,c=3, d=6, e, f;
+            b+=b;
+            a-=a;
+            c*=c;
+            d/=d;
+            e=a%b;
+
+
+
+            System.out.println("Addition: "+b);
+            System.out.println("Subtraction: "+a);
+            System.out.println("Multiplication: "+c);
+            System.out.println("Division: "+d);
+            System.out.println("Modulus: "+e);
+
+
+        }
+    }
+
+
