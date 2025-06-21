0caa411b6ae07254a5498b5439e3c43dab4de72f
Mona1307 <monicaadhana05@gmail.com>
2025-06-21 12:36:42 +0530

    student sample 1

---
+++ src/Boolean.java
@@ -0,0 +1,59 @@
+public class Boolean {
+    public static void main(String[] args) {
+    int a=8,b= 4;
+   if (a==b) {
+       System.out.println("a==b:" +true);
+   }
+        else {
+           System.out.println("a==b:" +false);
+    }
+        if (a!=b) {
+            System.out.println("a!=b:" +true);
+        }
+        else {
+            System.out.println("a!=b:" +false);
+        }
+        if (a<b) {
+            System.out.println("a<b:" +true);
+        }
+        else {
+            System.out.println("a<b:" +false);
+        }
+        if (a>b) {
+            System.out.println("a>b:" +true);
+        }
+        else {
+            System.out.println("a>b:" +false);
+        }
+        if (a<=b) {
+            System.out.println("a<=b:" +true);
+        }
+        else {
+            System.out.println("a<=b:" +false);
+        }
+        if (a>=b) {
+            System.out.println("a>=b:" +true);
+        }
+        else {
+            System.out.println("a>=b:" +false);
+        }
+        if (a>b && a>0) {
+            System.out.println("a>b && a>0:" +true);
+        }
+        else {
+            System.out.println("a>b && a>0:" +false);
+        }
+        if (a > b) {// (a>0) {
+            System.out.println("a>b // a>0:" +true);
+        }
+        else {
+            System.out.println("a>b // a>0:" +false);
+        }
+        if (a!=b) {
+        System.out.println("a!b:" +true);
+    }
+        else {
+        System.out.println("a!b:" +false);
+    }
+}
+}
