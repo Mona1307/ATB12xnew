2a9fc0be6f8fbcedc1d773d4e1ea3a41d48ae004
Mona1307 <monicaadhana05@gmail.com>
2025-06-21 11:52:46 +0530

    student sample 1

--- src/Propervardeclaration.java
+++ src/Propervardeclaration.java
@@ -4,10 +4,10 @@ public class Propervardeclaration {
         boolean myBool=true;
         char myLetter= 'M';
                 double myVal=24.2;
-        System.out.println(+a);
-        System.out.println(myBool);
-        System.out.println(myLetter);
-        System.out.println(myVal);
+        System.out.println("Integer: "+a);
+        System.out.println("Boolean: "+myBool);
+        System.out.println("Character: "+myLetter);
+        System.out.println("Double: "+myVal);
 
 
     }
