85dd3a1bcdc6d23fc76b8fddc7353ee0fd156fc4
Mona1307 <monicaadhana05@gmail.com>
2025-06-21 11:47:57 +0530

    student sample 1

---
+++ src/Propervardeclaration.java
@@ -0,0 +1,14 @@
+public class Propervardeclaration {
+    public static void main(String[] args) {
+        int a=5;
+        boolean myBool=true;
+        char myLetter= 'M';
+                double myVal=24.2;
+        System.out.println(+a);
+        System.out.println(myBool);
+        System.out.println(myLetter);
+        System.out.println(myVal);
+
+
+    }
+}
